35234e6d91dac967a228bf78c258c50f2a3b6c5d
mozartCuman <mozartcumana@gmail.com>
2025-01-31 17:50:03 -0300

    esqueleto do RiderX e uma menssagem para meus amigos

---
+++ riderxfront/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import index from 'index.css';
+
+function Home() {
+  return (
+    <div className="flex items-center justify-center h-screen animate-background-pulse">
+      <span className="text-6xl font-bold bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 text-transparent bg-clip-text animate-pulse">
+        Mozart é brabo!!!
+      </span>
+    </div>
+  );
+}
+
+export default Home;
